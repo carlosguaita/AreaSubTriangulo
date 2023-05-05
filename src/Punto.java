3649f96d588462d4bfc78fb5d722a6cee39dcd91
carlos.guaita <carlos.guaita@udla.edu.ec>
2023-05-05 09:43:56 -0500

    Resuelto

--- src/Punto.java
+++ src/Punto.java
@@ -1,3 +1,28 @@
 public class Punto {
 
+    double x;
+    double y;
+
+    public Punto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+
 }
